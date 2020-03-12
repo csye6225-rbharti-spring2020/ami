8383a6ca55cc9566ed1535a48b2148d4c0980133
Rohan Bharti <bharti.r@northeastern.edu>
2020-03-12 12:18:42 -0400

    updated java and mvn sh file

--- scripts/JdkAndMavenSetup.sh
+++ scripts/JdkAndMavenSetup.sh
@@ -5,9 +5,10 @@ sudo apt upgrade -y
 sudo apt --fix-broken install
 
 #JAVA SETUP
-sudo apt install software-properties-common apt-transport-https -y
-sudo add-apt-repository ppa:webupd8team/java -y
-sudo apt install oracle-java8-installer -y
+sudo add-apt-repository ppa:linuxuprising/java -y
+sudo apt update -y
+sudo apt install oracle-java11-installer -y
+sudo apt install oracle-java11-set-default -y
 java -version
 echo "export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))" | sudo tee -a /etc/profile
 
@@ -22,18 +23,23 @@ echo "export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.
 source /etc/profile
 
 #install maven
-cd /opt/
-wget http://www-eu.apache.org/dist/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz
-tar -xf apache-maven-3.5.3-bin.tar.gz
-mv apache-maven-3.5.3/ apache-maven/
+cd /usr/local
+wget https://www-eu.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
+sudo tar xzf apache-maven-3.6.0-bin.tar.gz
+sudo ln -s apache-maven-3.6.0 apache-maven
 
 #import profile
 source /etc/profile
 
 #export path
-sudo echo "export M2_HOME=/opt/apache-maven" | sudo tee -a /etc/profile
-sudo echo 'export MAVEN_HOME=/opt/apache-maven' | sudo tee -a /etc/profile
+sudo echo "export M2_HOME=/usr/local/apache-maven" | sudo tee -a /etc/profile
+sudo echo 'export MAVEN_HOME=/usr/local/apache-maven' | sudo tee -a /etc/profile
 echo "export PATH=${M2_HOME}/bin:${PATH}" | sudo tee -a /etc/profile
 
 #import updated profile
 source /etc/profile
+
+mvn -version
+
+#removing the tar file
+rm -f apache-maven-3.6.0-bin.tar.gz
